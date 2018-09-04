62aa139944fcc3de2bbee7ed528d34681cb83f93
ksenia <uzlovakip@gmail.com>
2018-09-04 11:03:51 +0300

    init

---
+++ src/app/entities/task.ts
@@ -0,0 +1,6 @@
+export class Task {
+    id: number;
+    title: string;
+    desc: string;
+    status: string;
+}
---
+++ src/app/mock-todos.ts
@@ -0,0 +1,14 @@
+const todos = [
+    {
+        id: 1,
+        title: 'Написать todos',
+        desc: 'Написать todos на angular',
+        status: 'active',
+    },
+    {
+        id: 2,
+        title: 'Изучить angular',
+        desc: 'Описание изучить angular',
+        status: 'active',
+    },
+]
